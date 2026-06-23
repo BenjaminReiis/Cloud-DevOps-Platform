a7cc0150faf8a4ff381c6504c10288f9a6ebaf47
Benjamin Reis <yasmindosireis@gmail.com>
2026-06-23 00:11:48 -0300

    Create cloud_sql.tf

---
+++ terraform/cloud_sql.tf
@@ -0,0 +1,28 @@
+resource "google_sql_database_instance" "postgres" {
+  name             = "app-postgres-instance"
+  database_version = "POSTGRES_15"
+  region           = var.region
+
+  settings {
+    tier = "db-f1-micro"
+
+    ip_configuration {
+      ipv4_enabled = true
+    }
+  }
+
+  deletion_protection = false
+
+  depends_on = [google_project_service.services]
+}
+
+resource "google_sql_database" "database" {
+  name     = var.db_name
+  instance = google_sql_database_instance.postgres.name
+}
+
+resource "google_sql_user" "users" {
+  name     = var.db_user
+  instance = google_sql_database_instance.postgres.name
+  password = var.db_password
+}
