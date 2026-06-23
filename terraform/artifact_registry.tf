62718cfc1dacc47aac28bc843c9986736f0b30e4
Benjamin Reis <yasmindosireis@gmail.com>
2026-06-23 00:11:25 -0300

    Create artifact_registry.tf

---
+++ terraform/artifact_registry.tf
@@ -0,0 +1,8 @@
+resource "google_artifact_registry_repository" "repo" {
+  location      = var.region
+  repository_id = var.artifact_registry_repo
+  description   = "Repositorio Docker para aplicacoes"
+  format        = "DOCKER"
+
+  depends_on = [google_project_service.services]
+}
